8e4a3398f91371ab75c4fc8da4f33b43fc8c1717
Amina <Vous@exemple.cocd>
2025-11-20 11:22:15 +0100

    premiere version

---
+++ ProxiBanqueSI_Amina_Serrano2/src/main/java/org/formation/hibernate/proxibanquesi_amina_serrano2/ProxiBanqueSiAminaSerrano2Application.java
@@ -0,0 +1,13 @@
+package org.formation.hibernate.proxibanquesi_amina_serrano2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueSiAminaSerrano2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanqueSiAminaSerrano2Application.class, args);
+    }
+
+}
---
+++ ProxiBanqueSI_Amina_Serrano2/src/test/java/org/formation/hibernate/proxibanquesi_amina_serrano2/ProxiBanqueSiAminaSerrano2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.hibernate.proxibanquesi_amina_serrano2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiBanqueSiAminaSerrano2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
